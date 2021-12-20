3adafcb9a5833fa8fc9f857b2004c7cdad7c7fa6
jascuas <loljavierdelago@hotmail.com>
2021-12-20 19:49:52 +0100

    add connect button for metamask

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,87 @@
-import React, { useEffect, useState, Component  } from "react";
+import React, { useEffect, useState } from "react";
 import Web3 from "web3";
 import './App.css';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
+
   const [web3Api, setWeb3Api] = useState({
     provider: null,
-    we3: null
-  })
+    web3: null
+  });
+
+  const [account, setAccount] = useState(null);
+
 
   useEffect(() => {
     const loadProvider = async () => {
-      let provider = null;
+      const provider = await detectEthereumProvider();
 
-      if (window.ethereum) {
-        provider = window.ethereum;
-      } else if (window.web3) {
-        provider = window.web3.currentProvider;
-      } else if (!process.env.production) {
-        provider = new Web3.provider.HtppProvider("http://192.168.1.0:7545");
+      if (provider) {
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider
+        })
+        console.log('Ethereum successfully detected!')
+        // From now on, this should always be true:
+        // provider === window.ethereum
+
+        // Access the decentralized web!
+      } else {
+
+        // if the provider is not detected, detectEthereumProvider resolves to null
+        console.error('Please install MetaMask!')
       }
+
 
     }
 
     loadProvider();
   }, [])
-  
-    return (
-      <div className="faucet-wrapper">
-        <div className="faucet">
-          <div className="balance-view is-size-2">
-            Current Balance: <strong>10</strong> ETH
-          </div>
-          <button className="btn mr-2"
-            onClick={async () => {
-              const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
-              console.log(accounts)
-            }}
-          >Connect</button>
-          <button className="btn mr-2">Donate</button>
-          <button className="btn">Withdraw</button>
-        </div>
-      </div>
-    );
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts()
+      setAccount(accounts[0])
+    }
+
+    web3Api.web3 && getAccount()
+  }, [web3Api.web3])
+
+  async function loadAccount() {
+    let button = document.querySelector("#Connect");
+    button.className = "button is-info is-loading mr-2"
+    const accounts = await web3Api.provider.request({ method: 'eth_requestAccounts' }).then(() => {
+      button.className = "button is-info mr-2"
+    });
   }
 
-  export default App;
+  return (
+    <div className="faucet-wrapper ">
+      <div className="faucet">
+        <div className="is-flex is-aling-items-center">
+          <span className="mr-2">
+            <strong>Account: </strong>
+          </span>
+          <h1>
+            {account ?
+              account :
+              <button
+                id="Connect"
+                className="button is-small mr-2"
+                onClick={loadAccount}
+              >Connect Wallet</button>}
+          </h1>
+        </div>
+        <div className="balance-view is-size-2 my-4">
+          Current Balance: <strong>10</strong> ETH
+        </div>
+
+        <button className="button is-link mr-2">Donate</button>
+        <button className="button is-primary">Withdraw</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
